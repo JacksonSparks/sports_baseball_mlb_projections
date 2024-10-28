dc34e9ded3161f8f0e919272452c67c7a81b7a84
Jackson Sparks <sparks.jackson.s@gmail.com>
2024-10-28 14:30:42 -0400

    Update mlb_battervteam_hth_data_f.py

--- mlb_battervteam_hth_data_f.py
+++ mlb_battervteam_hth_data_f.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def merge_batter_team_matchups():
     # Load the CSV files into DataFrames
-    team_matchups_df = pd.read_csv('batter_team_matchups_e.csv')
-    pitcher_matchups_df = pd.read_csv('batter_pitcher_matchups_d.csv')
+    team_matchups_df = pd.read_csv('mlb_battervteam_hth_data_e.csv')
+    pitcher_matchups_df = pd.read_csv('mlb_battervpitcher_hth_data_d.csv')
 
     # Merge the DataFrames on the "Batter" column
     merged_df = team_matchups_df.merge(pitcher_matchups_df[['Batter', 'Pitching Outs']],
@@ -11,11 +11,11 @@ def merge_batter_team_matchups():
                                        how='left')
 
     # Save the merged DataFrame to a new CSV file
-    merged_df.to_csv('batter_team_matchups_f.csv', index=False)
+    merged_df.to_csv('mlb_battervteam_hth_data_f.csv', index=False)
 
 def main():
     merge_batter_team_matchups()
-    print("The CSV files have been successfully merged and saved as 'batter_team_matchups_f.csv'.")
+    print("The CSV files have been successfully merged and saved as 'mlb_battervteam_hth_data_f.csv'.")
 
 if __name__ == "__main__":
     main()
